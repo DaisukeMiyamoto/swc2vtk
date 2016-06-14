BUG: rotation angle is not correst, again

swc2vtk.py
@@ -28,8 +28,49 @@ DATASET UNSTRUCTURED_GRID
     def add_point(self, x, y, z):
         self.point_list.append([x, y, z])
         
+        
+    def _gen_cylinder_point(self, div=10):
 
-    def add_cube(self, x=0, y=0, z=0, size=1.0, data=1.0):
+        point_list = []
+        point_order = []
+        for i in range(div):
+            theta = float(i) / div * 2. * np.pi
+            point_list.extend([[0, np.cos(theta), np.sin(theta)], [1, np.cos(theta), np.sin(theta)]])
+            
+            point_order.extend([len(point_order), len(point_order)+1])
+   
+   
+        point_order.extend([0, 1])
+        return(point_order, np.array(point_list))
+        
+        
+    def add_cylinder(self, pos_x=0, pos_y=0, pos_z=0, radius=1.0, height=1.0, rot_y=0, rot_z=0, data=0.0):
+
+        point_start = len(self.point_list)
+        
+        points, local_point_list = self._gen_cylinder_point()
+        points = [i+point_start for i in points]
+
+        # scale
+        local_point_list = np.array([ v*[height, radius, radius] for v in local_point_list])
+
+        # rot z
+        local_point_list = np.array([ [v[0]*np.cos(rot_z)-v[1]*np.sin(rot_z), v[0]*np.sin(rot_z)+v[1]*np.cos(rot_z), v[2]] for v in local_point_list])
+
+        # rot y
+        local_point_list = np.array([ [v[0]*np.cos(rot_y)+v[2]*np.sin(rot_y), v[1], -v[0]*np.sin(rot_y)+v[2]*np.cos(rot_y)] for v in local_point_list])
+
+        # move
+        local_point_list = np.array([ v+[pos_x, pos_y, pos_z] for v in local_point_list])
+
+
+        self.point_list.extend(local_point_list)
+
+        cell = {'type':6, 'points':points, 'data':data}        
+        self.cell_list.append(cell)
+
+
+    def add_cube(self, x=0, y=0, z=0, size=1.0, data=0.0):
         self.add_cuboid(x, y, z, size, size, size, 0.0, 0.0, data)
 
 
@@ -64,7 +105,7 @@ DATASET UNSTRUCTURED_GRID
         self.cell_list.append(cell)
 
 
-    def add_cuboid_p2p(self, pos1=[0,0,0], pos2=[2,0,0], size=1.0, data=0):
+    def add_cuboid_p2p(self, pos1=[0,0,0], pos2=[2,0,0], size=1.0, data=0, draw_type=0):
 
         pos1 = np.array(pos1)
         pos2 = np.array(pos2)
@@ -77,7 +118,8 @@ DATASET UNSTRUCTURED_GRID
         
         len = np.sqrt(local_pos[0]**2 + local_pos[1]**2 + local_pos[2]**2)
 
-        self.add_cuboid(pos1[0], pos1[1], pos1[2], len, size , size, rot_y, rot_z, data)
+        #self.add_cuboid(pos1[0], pos1[1], pos1[2], len, size , size, rot_y, rot_z, data)
+        self.add_cylinder(pos1[0], pos1[1], pos1[2], size, len, rot_y, rot_z, data)
 
         
 
@@ -91,7 +133,6 @@ DATASET UNSTRUCTURED_GRID
         self.cell_list.append(cell)
 
 
-
     def add_swc_with_cuboid(self, swc_filename):
         self.swc_list.append(Swc(swc_filename))
         datasize = len(self.swc_list[-1].data)
@@ -99,7 +140,7 @@ DATASET UNSTRUCTURED_GRID
         for record in self.swc_list[-1].data.values():
             if record['parent'] > 0:                
                 parent_record = self.swc_list[-1].data[record['parent']]
-                self.add_cuboid_p2p(record['pos'], parent_record['pos'], record['radius']*2, float(record['id'])/datasize)
+                self.add_cuboid_p2p(record['pos'], parent_record['pos'], record['radius'], float(record['id'])/datasize)
 
 
     def add_swc_with_line(self, swc_filename):
@@ -173,28 +214,41 @@ DATASET UNSTRUCTURED_GRID
 if __name__ == '__main__':
 
 
-    stoptime=100
-    filename = 'swc.vtk'
-    filename_base = 'swc_cuboid%d.vtk'
-    vtkgen = VtkGenerator()
+    def test_swc_movie(stoptime=100):
+        filename = 'swc.vtk'
+        filename_base = 'swc_cuboid%d.vtk'
+        vtkgen = VtkGenerator()
+        
+        for i in range(10):
+            vtkgen.add_cube(i, i, 0, i*1, i*0.1)
     
-    for i in range(10):
-        vtkgen.add_cube(i, i, 0, i*1, i*0.1)
+        #vtkgen.add_swc(os.path.join('data', 'simple.swc'))
+        #vtkgen.add_swc_with_line(os.path.join('data', 'Swc_BN_1056.swc'))
+        #vtkgen.add_swc_with_cube(os.path.join('data', 'Swc_BN_1056.swc'))
+        vtkgen.add_swc_with_cuboid(os.path.join('data', 'Swc_BN_1056.swc'))
+    
+        for t in range(stoptime):
+            for i in range(len(vtkgen.cell_list)):
+                #vtkgen.cell_list[i]['data'] = vtkgen.cell_list[(i+1) % len(vtkgen.cell_list)]['data']
+                vtkgen.cell_list[i]['data'] += 0.01
+                if vtkgen.cell_list[i]['data'] > 1.0:
+                    vtkgen.cell_list[i]['data'] = 0.0
+                
+            vtkgen.write_vtk(filename_base % t)
+    
+    
+        #vtkgen.show_state()
+        #print vtkgen.swc_list[-1].data
+    
+    def test_cylinder():
+        filename = 'cylinder.vtk'
+        vtkgen = VtkGenerator()
 
-    #vtkgen.add_swc(os.path.join('data', 'simple.swc'))
-    #vtkgen.add_swc_with_line(os.path.join('data', 'Swc_BN_1056.swc'))
-    #vtkgen.add_swc_with_cube(os.path.join('data', 'Swc_BN_1056.swc'))
-    vtkgen.add_swc_with_cuboid(os.path.join('data', 'Swc_BN_1056.swc'))
 
-    for t in range(stoptime):
-        for i in range(len(vtkgen.cell_list)):
-            #vtkgen.cell_list[i]['data'] = vtkgen.cell_list[(i+1) % len(vtkgen.cell_list)]['data']
-            vtkgen.cell_list[i]['data'] += 0.01
-            if vtkgen.cell_list[i]['data'] > 1.0:
-                vtkgen.cell_list[i]['data'] = 0.0
-            
-        vtkgen.write_vtk(filename_base % t)
-
-
-    #vtkgen.show_state()
-    #print vtkgen.swc_list[-1].data
+        for i in range(10):
+            vtkgen.add_cylinder(i*5, 0, 0, i*0.1, i*2, np.pi/2.0 + i*0.1, 0, 0.2*i)
+        
+        vtkgen.write_vtk(filename)
+        
+    test_cylinder()
+    test_swc_movie(10)