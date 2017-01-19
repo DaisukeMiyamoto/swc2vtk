933cc41654eea3a5e274b3bb7757aee854ab396c
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2017-01-19 15:36:46 +0900

    write a test for add_swc with reading file

--- tests/test_vtkgenerator.py
+++ tests/test_vtkgenerator.py
@@ -15,13 +15,19 @@ class TestVtkGenerator(unittest.TestCase):
     def setUp(self):
         self.vtk_generator = swc2vtk.VtkGenerator()
 
+    def tearDown(self):
+        self.vtk_generator.write_vtk('test.vtk')
+
     def test_add_cylinder(self):
         self.vtk_generator.add_cylinder()
-        self.vtk_generator.write_vtk('test.vtk')
 
     def test_add_sphere(self):
         self.vtk_generator.add_sphere()
-        self.vtk_generator.write_vtk('test.vtk')
+
+    def test_add_swc(self):
+        simple_cmp_size = 11
+        self.vtk_generator.add_swc('simple.swc')
+        self.assertEqual(simple_cmp_size, len(self.vtk_generator.swc_list[0].data))
 
 
 def suite():
